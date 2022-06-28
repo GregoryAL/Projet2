6ae6dc0aec8ce53d4b02de16f9cbea25fbf9fb1b
Gregory ALLEN <gregory.allen.larut@live.com>
2022-06-28 18:13:54 +0200

    Creation du fichier python principal et structuration des étapes de récupération des informations d'une page

---
+++ AnalyseBooksToScrape.py
@@ -0,0 +1,21 @@
+def main():
+
+    """Point d'entrée du programme de scrapping"""
+    # Creer un csv avec les entetes indiquées
+
+    # BookURL = recupere l'url de la page d'un livre
+    # BookURLScrapped = Recupere le resultat de scrapping et parsing de la page URLlivre
+    # BookUPC = Recupere l'information dans les données scrappées et la clean
+    # BookTitle = Recupere l'information dans les données scappées et la clean
+    # BookPriceWithTax = Recupere l'information dans les données scappées et la clean
+    # BookPriceWithoutTax = Recupere l'information dans les données scappées et la clean
+    # BookNumberAvailable = Recupere l'information dans les données scappées et la clean
+    # BookDescription = Recupere l'information dans les données scappées et la clean
+    # BookCategory =  Recupere l'information dans les données scappées et la clean
+    # BookReviewRating =  Recupere l'information dans les données scappées et la clean
+    # BookImageUrl =  Recupere l'information dans les données scappées et la clean
+    # Ajouter les informations au CSV
+
+
+if __name__ == "__main__":
+    main()
