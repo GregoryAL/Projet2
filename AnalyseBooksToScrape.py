d3dde6ad95cc81d6f73560a63254f28d66b3d02d
Gregory ALLEN <gregory.allen.larut@live.com>
2022-06-29 18:10:15 +0200

    Ajout de la fonction de recuperation du UPC (RecuperationLigneNumUPC) Ajout de la fonction cleanant les chaines <td></td> Ajout des docstrings pour les fonctions

--- AnalyseBooksToScrape.py
+++ AnalyseBooksToScrape.py
@@ -5,22 +5,36 @@ from bs4 import BeautifulSoup
 
 
 def erreurscrapping(URLnonJoignable):
+    """Ajoute l url de la page en erreur au fichier erreur.txt"""
     fichiererreur = open('erreur.txt', "a")
     fichiererreur.write(URLnonJoignable + ', ')
     fichiererreur.close()
 
 def RecuperationEtParsing(URLaScrapperEtParser):
+    """Recupere la page en argument, verifie le bon fonctionnement, et parse la page. Renvoie la page parsée"""
     ScrappedPage = requests.get(URLaScrapperEtParser)
 
     if ScrappedPage.status_code == 200:
         soup = BeautifulSoup(ScrappedPage.content, 'html.parser')
-        print('Scrapping ok')
         return(soup)
 
     else:
         print('un probleme a été rencontré avec ', URLaScrapperEtParser, 'Passage au livre suivant. ', URLaScrapperEtParser,
               ' est loggé dans le fichier erreur.txt')
         erreurscrapping(URLaScrapperEtParser)
+
+def RecuperationLigneNumUPC(ScrappedContent):
+    """Recupere le numéro UPC de la page precedement scrappée et parsée"""
+    UPCLigne=ScrappedContent.find(string="UPC")
+    UPCnumLigne = UPCLigne.next_element
+    return UPCnumLigne
+
+def CleanLignetd(LigneACleaner):
+    """Clean la chaine de caractere pour ne garder que le contenu et supprimer les balises html"""
+    contenu = str(LigneACleaner)
+    contenu = contenu.replace('<td>', '')
+    contenu = contenu.replace('</td>', '')
+    return contenu
 
 def main():
     """Point d'entrée du programme de scrapping"""
@@ -34,11 +48,12 @@ def main():
     BookURL = ('http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html')
     # BookURLScrapped = Vérifie et Recupere le resultat de scrapping et parsing de la page URLlivre
     ScrappingPage = RecuperationEtParsing(BookURL)
-
-
     # BookUPC = Recupere l'information dans les données scrappées et la clean
+    TDdeUPC = RecuperationLigneNumUPC(ScrappingPage)
+    BookUPC = CleanLignetd(TDdeUPC)
 
     # BookTitle = Recupere l'information dans les données scappées et la clean
+
     # BookPriceWithTax = Recupere l'information dans les données scappées et la clean
     # BookPriceWithoutTax = Recupere l'information dans les données scappées et la clean
     # BookNumberAvailable = Recupere l'information dans les données scappées et la clean
