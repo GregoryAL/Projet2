6683f060ffa0b3b80a327701fe6d9086b0a587b5
Gregory ALLEN <gregory.allen.larut@live.com>
2022-06-29 15:53:57 +0200

    Imports des librairies Requests et BeautifulSoup Récupération des données d'une URL prédéfinie (car 1ere phase), Vérification de cette récupération (RecuperationEtParsing), Si erreur ajout de l url en défaut dans un fichier erreur.txt qui est reinitialisé à chaque lancement du programme Mise à jour de gitignore pour exclure le fichier erreur.txt

--- AnalyseBooksToScrape.py
+++ AnalyseBooksToScrape.py
@@ -1,11 +1,43 @@
-def main():
+# Imports des librairies
 
+import requests
+from bs4 import BeautifulSoup
+
+
+def erreurscrapping(URLnonJoignable):
+    fichiererreur = open('erreur.txt', "a")
+    fichiererreur.write(URLnonJoignable + ', ')
+    fichiererreur.close()
+
+def RecuperationEtParsing(URLaScrapperEtParser):
+    ScrappedPage = requests.get(URLaScrapperEtParser)
+
+    if ScrappedPage.status_code == 200:
+        soup = BeautifulSoup(ScrappedPage.content, 'html.parser')
+        print('Scrapping ok')
+        return(soup)
+
+    else:
+        print('un probleme a été rencontré avec ', URLaScrapperEtParser, 'Passage au livre suivant. ', URLaScrapperEtParser,
+              ' est loggé dans le fichier erreur.txt')
+        erreurscrapping(URLaScrapperEtParser)
+
+def main():
     """Point d'entrée du programme de scrapping"""
+    # Initialisation d'un fichier erreur
+    fichierDErreur = open('erreur.txt', "w")
+    fichierDErreur.close()
+
     # Creer un csv avec les entetes indiquées
 
-    # BookURL = recupere l'url de la page d'un livre
-    # BookURLScrapped = Recupere le resultat de scrapping et parsing de la page URLlivre
+    # BookURL = recupere l'url de la page d'un livre : Phase 1 : URL prédeterminée
+    BookURL = ('http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html')
+    # BookURLScrapped = Vérifie et Recupere le resultat de scrapping et parsing de la page URLlivre
+    ScrappingPage = RecuperationEtParsing(BookURL)
+
+
     # BookUPC = Recupere l'information dans les données scrappées et la clean
+
     # BookTitle = Recupere l'information dans les données scappées et la clean
     # BookPriceWithTax = Recupere l'information dans les données scappées et la clean
     # BookPriceWithoutTax = Recupere l'information dans les données scappées et la clean
