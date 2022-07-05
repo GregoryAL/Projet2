e88fc7b38376e7ce7e82de388fc3eba68d22af33
Gregory ALLEN <gregory.allen.larut@live.com>
2022-07-05 15:10:32 +0200

    Ajout de test de validité pour les fonctions clean_balises et clean_resultat_xpath

--- AnalyseBooksToScrape.py
+++ AnalyseBooksToScrape.py
@@ -12,7 +12,7 @@ from pathlib import Path
 def erreurscrapping(urlnonjoignable):
     """Ajoute l url de la page en erreur au fichier erreur.txt"""
     fichiererreur = open('donnees/erreur.txt', "a")
-    fichiererreur.write(urlnonjoignable + ', ')
+    fichiererreur.write("l'url " + urlnonjoignable + "n'est pas joignable" + "\n")
     fichiererreur.close()
     exit()
 
@@ -46,14 +46,30 @@ def recuperation_et_parsing_lxml(url_a_scrapper_et_parser):
 
 def clean_balises(ligne_a_cleaner, balise_html):
     """Clean la chaine de caractere pour ne garder que le contenu et supprimer les balises html"""
-    contenu = str(ligne_a_cleaner).replace('<'+balise_html+'>', '').replace('</'+balise_html+'>', '')
+    balise_debut = '<' + balise_html + '>'
+    balise_fin = '</' + balise_html + '>'
+    if str(ligne_a_cleaner).startswith(str(balise_debut)) and str(ligne_a_cleaner).endswith(str(balise_fin)):
+        contenu = str(ligne_a_cleaner).replace('<'+balise_html+'>', '').replace('</'+balise_html+'>', '')
+    else:
+        fichiererreur = open('donnees/erreur.txt', "a")
+        fichiererreur.write("la balise" + balise_html + "était attendue pour l'information" + ligne_a_cleaner + "\n")
+        fichiererreur.close()
+        contenu = ligne_a_cleaner
     return contenu
 
 
 def clean_resultat_xpath(resultat):
     """ renvoie la valeur recuperée avec xpath en supprimant les crochets et ' """
-    resultat = str(resultat).strip(" [']")
-    return resultat
+    if str(resultat).startswith("['") and str(resultat).endswith("']"):
+        resultat_cleaned = str(resultat).strip(" [']")
+    elif str(resultat).startswith('["') and str(resultat).endswith('"]'):
+        resultat_cleaned = str(resultat).strip(' ["]')
+    else:
+        fichiererreur = open('donnees/erreur.txt', "a")
+        fichiererreur.write("les caracteres ['...'] étaient attendus pour l'information" + str(resultat) + "\n")
+        fichiererreur.close()
+        resultat_cleaned = resultat
+    return resultat_cleaned
 
 
 def generation_nom_csv(categorie):
@@ -79,28 +95,28 @@ def recuperation_info_livre(url_du_livre):
     """recupere les differentes infos necessaires et les renvoi """
     # BookURL = recupere l'url de la page d'un livre : Phase 1 : URL prédeterminée
     book_url = url_du_livre
-    liste_des_infos = str(book_url)
+    liste_infos_format_liste = [book_url]
     # BookURLScrapped = Vérifie et Recupere le resultat de scrapping et parsing de la page URLlivre
     scrapped_page_bs4 = recuperation_et_parsing(book_url)
     scrapped_page_lxml = recuperation_et_parsing_lxml(book_url)
     # BookUPC = Recupere l'information dans les données scrappées et la clean
     book_upc = scrapped_page_bs4.find("table", {"class": "table table-striped"}).find(string="UPC").find_next('td')
     book_upc = clean_balises(book_upc, 'td')
-    liste_des_infos = liste_des_infos+'²'+str(book_upc)
+    liste_infos_format_liste.append(book_upc)
     # BookTitle = Recupere l'information dans les données scappées et la clean
     book_title = scrapped_page_bs4.find("div", {"class": "col-sm-6 product_main"}).find('h1')
     book_title = clean_balises(book_title, 'h1')
-    liste_des_infos = liste_des_infos+'²'+str(book_title)
+    liste_infos_format_liste.append(book_title)
     # BookPriceWithTax = Recupere l'information dans les données scappées et la clean
     book_price_with_tax = scrapped_page_bs4.find("table", {"class": "table table-striped"}).\
         find(string="Price (incl. tax)").find_next('td')
     book_price_with_tax = clean_balises(book_price_with_tax, 'td').strip('£')
-    liste_des_infos = liste_des_infos+'²'+str(book_price_with_tax)
+    liste_infos_format_liste.append(book_price_with_tax)
     # BookPriceWithoutTax = Recupere l'information dans les données scappées et la clean
     book_price_without_tax = scrapped_page_bs4.find("table", {"class": "table table-striped"}).\
         find(string="Price (excl. tax)").find_next('td')
     book_price_without_tax = clean_balises(book_price_without_tax, 'td').strip('£')
-    liste_des_infos = liste_des_infos+'²'+str(book_price_without_tax)
+    liste_infos_format_liste.append(book_price_without_tax)
     # BookNumberAvailable = Recupere l'information dans les données scappées et la clean
     book_number_available = scrapped_page_bs4.find("table", {"class": "table table-striped"}).\
         find(string="Availability").find_next('td')
@@ -110,7 +126,7 @@ def recuperation_info_livre(url_du_livre):
         if book_number_available[i].isdigit():
             nombre_a_renvoyer = nombre_a_renvoyer + book_number_available[i]
     book_number_available = nombre_a_renvoyer
-    liste_des_infos = liste_des_infos+'²'+str(book_number_available)
+    liste_infos_format_liste.append(book_number_available)
     # BookDescription = Recupere l'information dans les données scappées et la clean
     # Verifie également qu'une description est disponible
     test_description = scrapped_page_lxml.xpath('//article[@class="product_page"]'
@@ -123,14 +139,14 @@ def recuperation_info_livre(url_du_livre):
         book_description = clean_resultat_xpath(book_description)
     else:
         book_description = "No Description Available"
-    liste_des_infos = liste_des_infos+'²'+str(book_description)
+    liste_infos_format_liste.append(book_description)
     # BookCategory =  Recupere l'information dans les données scappées et la clean
     book_category = scrapped_page_lxml.xpath('//div[@class="page_inner"]'
                                              '/ul[@class="breadcrumb"]'
                                              '/li[last()-1]'
                                              '/a/text()')
     book_category = clean_resultat_xpath(book_category)
-    liste_des_infos = liste_des_infos+'²'+str(book_category)
+    liste_infos_format_liste.append(book_category)
     # BookReviewRating =  Recupere l'information dans les données scappées et la clean
     book_rating = scrapped_page_lxml.xpath('//article[@class="product_page"]'
                                            '/div[@class="row"]'
@@ -153,7 +169,7 @@ def recuperation_info_livre(url_du_livre):
         book_rating = "0"
     else:
         book_rating = "None"
-    liste_des_infos = liste_des_infos+'²'+str(book_rating)
+    liste_infos_format_liste.append(book_rating)
     # BookImageUrl =  Recupere l'information dans les données scappées et la clean
     book_pic_url = scrapped_page_lxml.xpath('//article[@class="product_page"]'
                                             '/div[@class="row"]'
@@ -163,8 +179,8 @@ def recuperation_info_livre(url_du_livre):
                                             )
     book_pic_url = clean_resultat_xpath(book_pic_url)
     book_pic_url = str(book_pic_url).replace('../..', 'http://books.toscrape.com')
-    liste_des_infos = liste_des_infos+'²'+str(book_pic_url)
-    return liste_des_infos
+    liste_infos_format_liste.append(book_pic_url)
+    return liste_infos_format_liste
 
 
 def renvoi_nombre_de_page_par_categorie(url_page):
@@ -180,20 +196,17 @@ def renvoi_nombre_de_page_par_categorie(url_page):
     base_url = url_page.replace('index.html', '')
     while presence_next == ['next']:
         nombre_de_page = nombre_de_page + 1
-        suffixe_url = clean_resultat_xpath(next_url_parsed.xpath(
-                                                                '//div[@class="page_inner"]'
-                                                                '//ul[@class="pager"]'
-                                                                '/li[@class="next"]/a/@href'))
+        suffixe_url = clean_resultat_xpath(next_url_parsed.xpath('//div[@class="page_inner"]'
+                                                                 '//ul[@class="pager"]'
+                                                                 '/li[@class="next"]/a/@href'))
         prochaine_page_categorie = base_url + suffixe_url
         liste_url_livres.append(prochaine_page_categorie)
         next_url_parsed = recuperation_et_parsing_lxml(prochaine_page_categorie)
         presence_next = next_url_parsed.xpath('//div[@class="page_inner"]'
-                                         '//ul[@class="pager"]'
-                                         '/li[@class="next"]/a/text()')
-
-
-
+                                              '//ul[@class="pager"]'
+                                              '/li[@class="next"]/a/text()')
     return liste_url_livres
+
 
 def recuperation_books_url_from_page(category_url_page):
     """ liste toutes les url des livres d'une page de catégorie"""
@@ -205,33 +218,14 @@ def recuperation_books_url_from_page(category_url_page):
     total_url_page = []
     for single_book_url in books_url_from_category:
         total_url_page.append(str(single_book_url).replace('../../../', 'http://books.toscrape.com'
-                                                                                   '/catalogue/'))
+                                                           '/catalogue/'))
 
     return total_url_page
 
 
-"""def main():
-    # Pour test
-    # Initialisation d'un fichier erreur
-    fichier_d_erreur = open('donnees/erreur.txt', "w")
-    fichier_d_erreur.close()
-    # Creer un csv avec les entetes indiquées à la date du jour
-    nom_fichier_csv = generation_nom_csv()
-    create_csv_jour(nom_fichier_csv)
-    liste_info = recuperation_info_livre('http://books.toscrape.com/catalogue/alice-in-wonderland-alices-adventures-in-
-    wonderland-1_5/index.html')
-    #liste_info = recuperation_info_livre(
-    #    'http://books.toscrape.com/catalogue/in-the-woods-dublin-murder-squad-1_433/index.html')
-    # Ajouter les informations au CSV
-    with open(nom_fichier_csv, 'a', newline='', encoding='utf-8') as dico_csv:
-        writercsv = csv.writer(dico_csv, delimiter='²', quotechar='|')
-        writercsv.writerow([liste_info])
-    print(liste_info)"""
-
-
 """Point d'entrée du programme de scrapping"""
 
-#Creation d'un dossier donnees s'il n'existe pas à la racine du projet
+# Creation d'un dossier donnees s'il n'existe pas à la racine du projet
 chemin_donnees = Path.cwd() / "donnees"
 chemin_donnees.mkdir(exist_ok=True)
 
@@ -245,16 +239,16 @@ site_scrapped_lxml = recuperation_et_parsing_lxml(site_url)
 categories_dictionnaire = {}
 urls_of_categories = []
 urls_of_categories_brut = site_scrapped_lxml.xpath('//div[@class="side_categories"]'
-                                           '/ul[@class="nav nav-list"]/li/ul/li/a/@href'
-                                           )
+                                                   '/ul[@class="nav nav-list"]/li/ul/li/a/@href'
+                                                   )
 for urls in urls_of_categories_brut:
     urls_of_categories.append("http://books.toscrape.com/" + str(urls))
 name_of_categories = []
 name_of_categories_brut = site_scrapped_lxml.xpath('//div[@class="side_categories"]'
-                                           '/ul[@class="nav nav-list"]/li/ul/li/a/text()'
-                                           )
+                                                   '/ul[@class="nav nav-list"]/li/ul/li/a/text()'
+                                                   )
 for each_name in name_of_categories_brut:
-    name_of_categories.append(each_name.replace(" ","").replace("\n", ""))
+    name_of_categories.append(each_name.replace(" ", "").replace("\n", ""))
 for each_url in urls_of_categories:
     index_list = urls_of_categories.index(each_url)
     categories_dictionnaire[name_of_categories[index_list]] = each_url
@@ -267,7 +261,6 @@ for categorie_name in categories_dictionnaire:
     # Lance fonction de recherche de la liste des urls d une categorie
     liste_url_categorie = renvoi_nombre_de_page_par_categorie(categories_dictionnaire[categorie_name])
     # Lance fonction de recuperation des url des livres des pages d'une catégories dans une liste
-    print(liste_url_categorie)
     for liste_url in liste_url_categorie:
         books_url_list_for_category = recuperation_books_url_from_page(liste_url)
         # Lance fonction recuperation d'information et ajout dans le csv a partir d une url
@@ -277,5 +270,4 @@ for categorie_name in categories_dictionnaire:
             fichier_chemin_complet = Path(categorie_path) / nom_fichier_csv
             with open(fichier_chemin_complet, 'a', newline='', encoding='utf-8') as dico_csv:
                 writercsv = csv.writer(dico_csv, delimiter='²', quotechar='|')
-                writercsv.writerow([liste_info])
-
+                writercsv.writerow(liste_info)
